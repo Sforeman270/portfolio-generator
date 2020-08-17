72535eacde3e62a81d413acf081376878b3f26b6
Sforeman270 <65334884+Sforeman270@users.noreply.github.com>
2020-08-16 20:55:42 -0500

    added inquirer

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
  
-const fs = require('fs');
- const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+// const fs = require('fs');
+//  const generatePage = require('./src/page-template.js');
+// const profileDataArgs = process.argv.slice(2);
+// const [name, github] = profileDataArgs;
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
-    console.log('Portfolio complete! Checkout index.html to see the output!');
-});
+// fs.writeFile('index.html', generatePage(name, github), err => {
+    // if (err) throw new Error(err);
+    // console.log('Portfolio complete! Checkout index.html to see the output!');
+// });
+
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+
